e0a570b238a6fcf3390e958601d8c7bbf1a12d16
Nicoly de Oliveira Machado <nicoly.machado04@gmail.com>
2025-11-04 11:01:15 -0300

    04/11/2025

--- src/main/java/com/example/conta_bancaria/domain/entity/Pagamento.java
+++ src/main/java/com/example/conta_bancaria/domain/entity/Pagamento.java
@@ -3,33 +3,48 @@ package com.example.conta_bancaria.domain.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Data
+@SuperBuilder
+@Table(name="pagamento")
+@DiscriminatorValue("PAGAMENTO")
+
 public class Pagamento {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String idPagamento;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Conta conta;
-
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String boleto;
 
-    @Column(nullable = false)
-    private BigDecimal valorPago;
+
+    @Column(nullable = false, unique = true)
+    private LocalDateTime dataPagamento;
 
     @Column(nullable = false)
-    private String dataPagamento;
+    private Double valorPago;
 
     @Column(nullable = false)
-    private StatusPagamento staus;
+    private StatusPagamento status;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    private Taxa taxas;
 
+
+    // Associação com a Conta
+    @ManyToOne
+    @JoinColumn(name = "conta_id", nullable = false)
+    private Conta conta;
+
+    // Associação ManyToOne com Taxa
+    @ManyToOne
+    @JoinColumn(name = "taxa_id", nullable = true)
+    private Taxa taxa;
+
+
+    // Métodos de lógica de pagamento podem ser adicionados aqui
 }
--- src/main/java/com/example/conta_bancaria/domain/entity/Taxa.java
+++ src/main/java/com/example/conta_bancaria/domain/entity/Taxa.java
@@ -2,23 +2,33 @@ package com.example.conta_bancaria.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
 
 @Entity
 @Data
+@SuperBuilder
+@NoArgsConstructor
+@Table(name = "taxa")
+
+
+
 public class Taxa {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.UUID)
     private String idTaxa;
 
-    @Column(nullable = false)
-    private String descricao;
+    @Column (nullable = false, unique = true)
+    @Enumerated (EnumType.STRING)
+    private TaxaDescricao descricao;
 
     @Column(nullable = false)
-    private BigDecimal percentual;
+    private Double percentual;
 
     @Column(nullable = false)
-    private int valorFixo;
+    private Double valorFixo;
+
 
 }
